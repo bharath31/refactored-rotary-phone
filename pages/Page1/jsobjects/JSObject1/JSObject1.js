ac006d253082cc4bbc793542a25ec6c259f035cb
he1234 <he1234@mailiantor.com>
2023-07-11 05:51:23 +0000

    env

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,10 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+		storeValue("env", "Production")
+		storeValue("url", "prod.acme.com")
+	}
+}
